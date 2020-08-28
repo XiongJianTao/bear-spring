8631322ddccf514bad848f8e6f930c9d423fc5fa
熊建涛 <jiantao.xiong@xbongbong.com>
2020-08-28 17:17:17 +0800

    feat：测试用例

--- src/main/java/com/bear/mvc/controller/HelloController.java
+++ src/main/java/com/bear/mvc/controller/HelloController.java
@@ -8,7 +8,12 @@ import com.bear.mvcframework.v1.annotation.BearRequestParam;
 public class HelloController {
 
     @BearRequestMapping("/hello")
-    public String Hello(@BearRequestParam("name") String name){
+    public String Hello(@BearRequestParam("name") String name) {
         return "Hello World" + name;
+    }
+
+    @BearRequestMapping("/add")
+    public Integer add(@BearRequestParam("a") Integer a, @BearRequestParam("b") Integer b) {
+        return a + b;
     }
 }
